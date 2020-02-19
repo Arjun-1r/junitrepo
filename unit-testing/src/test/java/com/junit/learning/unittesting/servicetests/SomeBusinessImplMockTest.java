5638a040a6d9a9905f46dc40c1646f73ed3ed116
arjun <arjunr681@gmail.com>
2020-02-19 09:19:55 +0530

    unit testing failing has been fixed.

--- unit-testing/src/test/java/com/junit/learning/unittesting/servicetests/SomeBusinessImplMockTest.java
+++ unit-testing/src/test/java/com/junit/learning/unittesting/servicetests/SomeBusinessImplMockTest.java
@@ -4,50 +4,36 @@ package com.junit.learning.unittesting.servicetests;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-
+import org.mockito.junit.MockitoJUnitRunner;
 import com.junit.learning.unittesting.dao.SomeDataService;
 import com.junit.learning.unittesting.services.SomeBusinessImpl;
-@ExtendWith(MockitoExtension.class)
 
+@RunWith(MockitoJUnitRunner.class)
 public class SomeBusinessImplMockTest {
 	   @InjectMocks
 	   SomeBusinessImpl sbi;
 	   @Mock
 	   SomeDataService dataServiceMock;
-	
-	//  @Before public void setup() {     MockitoAnnotations.initMocks(this); }
-	   
-	   @BeforeEach
-	   void setMockOnService() {
-		   MockitoAnnotations.initMocks(this);
-	   }
-	
+
 	@Test
-	void calculateSumUsingDataService_Basic() {
+	public void calculateSumUsingDataService_Basic() {
 	     when(dataServiceMock.retrieveAllDatas()).thenReturn(new int[] {2,3,5});
 	     assertEquals(10,sbi.calculateSumUsingDataService());
 	}
 	
 	
 	@Test
-	void calculateSumEmpty() {
+	public void calculateSumEmpty() {
 	   when(dataServiceMock.retrieveAllDatas()).thenReturn(new int[] {});
 	    assertEquals(0,sbi.calculateSumUsingDataService());
 	}
 	
 	@Test
-	void calculateSumOneValue() {
+	public void calculateSumOneValue() {
 	     when(dataServiceMock.retrieveAllDatas()).thenReturn(new int[] {5});
 	     assertEquals(5,sbi.calculateSumUsingDataService());
 	}
